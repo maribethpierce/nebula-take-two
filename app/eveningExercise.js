22b2878eb3928649948eeb1a203567ba18b06c1b
Maribeth <maribethpierce@gmail.com>
2015-11-25 14:38:43 -0500

    slight progress

--- app/eveningExercise.js
+++ app/eveningExercise.js
@@ -8,23 +8,35 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.eveningExerciseAnswers = {
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
-
+    var total = 0
+    for(i in arr) {
+        total+= arr[i];
+    };
+    return total;
   },
 
   remove : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i]===item) {
+        arr.splice(i, 1);
+      };
+    };
+    return arr;
   },
 
   reverseString : function(str) {
-
+    return str.split('').reverse().join('');
   },
 
   longestSubString : function(str) {
-
+    var list = str.split(' ')
+    for(word in list) {
+      
+    }
   },
 
   letterMoveForward : function(str) {
